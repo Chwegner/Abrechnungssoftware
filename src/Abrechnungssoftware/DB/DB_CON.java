6cb45965f73cc84d8665e99685839af39b84b117
Manuel Osterholzer <manuelosterholzer@web.de>
2019-01-14 20:11:01 +0100

    Db_CON Update für KundenListe und Kunden anlegen/löschen/ändern/anzeigen Installationsroutine hinzugefügt in MainClass und DB_CON für Stammdaten und Kunden Tabelle

--- src/Abrechnungssoftware/DB/DB_CON.java
+++ src/Abrechnungssoftware/DB/DB_CON.java
@@ -21,8 +21,7 @@ import java.util.List;
 import java.util.Properties;
 
 
-public class DB_CON
-{
+public class DB_CON {
 
     protected Connection connection;
     protected Statement statement;
@@ -34,16 +33,16 @@ public class DB_CON
     protected String pass;
     protected Reader reader = null;
     protected Stammdaten stamm;
+    protected Kunde kunde;
     protected ArrayList<Kunde> kundenliste = new ArrayList<Kunde>();
     protected ArrayList auftragliste;
     protected ArrayList rechnungliste;
 
 
-    public DB_CON()
-    {
+
+    public DB_CON(){
         this.stamm = stamm;
-        try
-        {
+        try {
             reader = new FileReader("system.ini");
             Properties prop = new Properties();
             prop.load(reader);
@@ -51,57 +50,45 @@ public class DB_CON
             this.user = prop.getProperty("USER");
             this.pass = prop.getProperty("PWD");
             this.dbName = prop.getProperty("DATENBANK");
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally
-        {
-            try
-            {
+        } finally {
+            try {
                 reader.close();
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
             }
         }
-        try
-        {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
 
-        } catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Fehler bei MySQL-JDBC-Bridge" + e);
             return;
         }
 
     }
 
-    /**
-     * Datenbankverbindung Öffnen
+    /** Datenbankverbindung Öffnen
+     *
      */
-    public void db_open()
-    {
-        try
-        {
+    public void db_open(){
+        try {
             String url = "jdbc:mysql://" + host + "/" + dbName;
             connection = DriverManager.getConnection(url, user, pass);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
     }
 
-    public Stammdaten LoadStammdaten(Stammdaten stamm)
-    {
-        try
-        {
+
+    public Stammdaten LoadStammdaten(Stammdaten stamm) {
+        try {
             statement = connection.createStatement();
             String sqlQuery = "SELECT * FROM stammdaten WHERE id = '1'";
             resultSet = statement.executeQuery(sqlQuery);
-            while (resultSet.next())
-            {
+            while (resultSet.next()) {
                 stamm.setId(resultSet.getInt("id"));
                 stamm.setFirma(resultSet.getString("firma"));
                 stamm.setVorname(resultSet.getString("vorname"));
@@ -123,57 +110,46 @@ public class DB_CON
             }
             statement.close();
 
-        } catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Fehler bei Abfrage: " + e);
         }
         return stamm;
     }
 
-    public void SaveStammdaten(Stammdaten daten)
-    {
-        try
-        {
+    public void SaveStammdaten(Stammdaten daten) {
+        try {
             statement = connection.createStatement();
             String sqlQuery = "UPDATE stammdaten SET " +
-                    "firma = '" + daten.getFirma() + "'," +
-                    "vorname = '" + daten.getVorname() + "'," +
-                    "nachname = '" + daten.getNachname() + "'," +
-                    "strasse = '" + daten.getStr() + "'," +
-                    "hsnr = '" + daten.getHsnr() + "'," +
-                    "plz = '" + daten.getPlz() + "'," +
-                    "ort = '" + daten.getOrt() + "'," +
-                    "telefon = '" + daten.getTelefon() + "'," +
-                    "telefax = '" + daten.getTelefax() + "'," +
-                    "web = '" + daten.getWeb() + "'," +
-                    "email = '" + daten.getEmail() + "'," +
-                    "bankname = '" + daten.getBankname() + "'," +
-                    "kontoinhaber = '" + daten.getKontoinhaber() + "'," +
-                    "bic = '" + daten.getBic() + "'," +
-                    "iban = '" + daten.getIban() + "'," +
-                    "steuernummer = '" + daten.getSteuernummer() + "' where id = '1'";
+                    "firma = '"+daten.getFirma()+"'," +
+                    "vorname = '"+daten.getVorname()+"'," +
+                    "nachname = '"+daten.getNachname()+"'," +
+                    "strasse = '"+daten.getStr()+"'," +
+                    "hsnr = '"+daten.getHsnr()+"'," +
+                    "plz = '"+daten.getPlz()+"'," +
+                    "ort = '"+daten.getOrt()+"'," +
+                    "telefon = '"+daten.getTelefon()+"'," +
+                    "telefax = '"+daten.getTelefax()+"'," +
+                    "web = '"+daten.getWeb()+"'," +
+                    "email = '"+daten.getEmail()+"'," +
+                    "bankname = '"+daten.getBankname()+"'," +
+                    "kontoinhaber = '" + daten.getKontoinhaber() +"'," +
+                    "bic = '"+daten.getBic()+"'," +
+                    "iban = '"+daten.getIban()+"'," +
+                    "steuernummer = '"+daten.getSteuernummer()+"' where id = '1'";
             statement.executeUpdate(sqlQuery);
             statement.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
     }
 
-    public ArrayList LoadKundenList()
-    {
-        try
-        {
+    public ArrayList LoadKundenList() {
+        try {
             statement = connection.createStatement();
-            String sqlQuery = "";
+            String sqlQuery = "SELECT * FROM kunden";
 
             resultSet = statement.executeQuery(sqlQuery);
-            while (resultSet.next())
-            {
-//                firma, name, vorname, strasse, hausnummer, ort, plz, telefon, fax, email,
-//                        apName, apVorname, apTelefon, apEmail;
-//                double
-//                        stundenSatz ;
+            while (resultSet.next()) {
                 Kunde kunde = new Kunde();
                 kunde.setId(resultSet.getInt("id"));
                 kunde.setFirma(resultSet.getString("firma"));
@@ -195,72 +171,211 @@ public class DB_CON
             }
             statement.close();
 
-        } catch (Exception e)
-        {
+        } catch (Exception e){
 
         }
         return kundenliste;
     }
 
-    public void EditKunde(int ID)
-    {
+    public void EditKunde(int ID, Kunde kunde) {
+        try {
+            statement = connection.createStatement();
+            String sqlQuery = "UPDATE kunden SET " +
+                    "firma = '"+kunde.getFirma()+"'," +
+                    "strasse = '"+kunde.getStrasse()+"'," +
+                    "hsnr = '"+kunde.getHausnummer()+"'," +
+                    "plz = '"+kunde.getHausnummer()+"'," +
+                    "ort = '"+kunde.getOrt()+"'," +
+                    "telefon = '"+kunde.getTelefon()+"'," +
+                    "telefax = '"+kunde.getFax()+"'," +
+                    "web = '"+kunde.getWeb()+"'," +
+                    "email = '"+kunde.getEmail()+"'," +
+                    "ap_anrede = '"+kunde.getAnrede()+"'," +
+                    "ap_vorname = '"+kunde.getApVorname()+"'," +
+                    "ap_nachname = '"+kunde.getApName()+"'," +
+                    "ap_telefon = '"+kunde.getApTelefon()+"'," +
+                    "ap_email = '"+kunde.getApEmail()+"'," +
+                    "stdsatz = '"+kunde.getStundenSatz()+"' " +
+                    "WHERE id = '"+ID+"'";
+
+            statement.executeUpdate(sqlQuery);
+            statement.close();
+
+        }catch (Exception e){
+
+        }
 
     }
 
-    public void NewKunde()
-    {
+    public void NewKunde(Kunde kunde) {
+        try {
+            statement = connection.createStatement();
+            String sqlQuery = "INSERT INTO kunden (" +
+                    "firma, strasse, hsnr, plz, ort, telefon, telefax, web, email, " +
+                    "ap_anrede, ap_vorname, ap_nachname, ap_telefon, ap_email, stdsatz) VALUES(" +
+                    "'"+kunde.getFirma()+"','"+kunde.getStrasse()+"','"+kunde.getHausnummer()+"'," +
+                    "'"+kunde.getPlz()+"','"+kunde.getOrt()+"','"+kunde.getTelefon()+"','"+kunde.getFax()+"'," +
+                    "'"+kunde.getWeb()+"','"+kunde.getEmail()+"','"+kunde.getAnrede()+"','"+kunde.getApVorname()+"'," +
+                    "'"+kunde.getApName()+"','"+kunde.getApTelefon()+"','"+kunde.getApEmail()+"'," +
+                    "'"+kunde.getStundenSatz()+"') ";
+
+            statement.executeUpdate(sqlQuery);
+            statement.close();
+        }catch (Exception e){
+
+        }
+    }
+
+    public void DeleteKunde(int ID) {
+        try{
+            statement = connection.createStatement();
+            String sqlQuery = "DELETE FROM kunden WHERE id = '"+ID+"'";
+            statement.executeUpdate(sqlQuery);
+            statement.close();
+
+        }catch(Exception e){
+
+        }
+    }
+
+    public Kunde LoadKunde(int ID) {
+        try{
+            statement = connection.createStatement();
+            String sqlQuery = "SELECT * FROM kunden WHERE id = '"+ID+"'";
+            resultSet = statement.executeQuery(sqlQuery);
+            while (resultSet.next()) {
+                kunde.setId(resultSet.getInt("id"));
+                kunde.setFirma(resultSet.getString("firma"));
+                kunde.setStrasse(resultSet.getString("strasse"));
+                kunde.setHausnummer(resultSet.getString("hsnr"));
+                kunde.setPlz(resultSet.getString("plz"));
+                kunde.setOrt(resultSet.getString("ort"));
+                kunde.setTelefon(resultSet.getString("telefon"));
+                kunde.setFax(resultSet.getString("telefax"));
+                kunde.setWeb(resultSet.getString("web"));
+                kunde.setEmail(resultSet.getString("email"));
+                kunde.setAnrede(resultSet.getString("ap_anrede"));
+                kunde.setApVorname(resultSet.getString("ap_vorname"));
+                kunde.setApName(resultSet.getString("ap_nachname"));
+                kunde.setApEmail(resultSet.getString("ap_email"));
+                kunde.setStundenSatz(resultSet.getDouble("stdsatz"));
+            }
+            statement.close();
+
+        }catch(Exception e){
+
+        }
+
+        return kunde;
+    }
+
+    public void NewAuftrag(int KD_ID,String von,String bis,String grund) {
 
     }
 
-    public void DeleteKunde(int ID)
-    {
-
-    }
-
-    public void LoadKunde(int ID)
-    {
-
-    }
-
-    public void NewAuftrag(int KD_ID, String von, String bis, String grund)
-    {
-
-    }
-
-    public ArrayList LoadAuftragList()
-    {
+    public ArrayList LoadAuftragList() {
 
         return auftragliste;
     }
 
-    public void LoadAuftrag(int ID)
-    {
+    public void LoadAuftrag(int ID) {
 
     }
 
-    public void DeleteAuftrag(int ID)
-    {
+    public void DeleteAuftrag(int ID) {
 
     }
 
-    public void EditAuftrag(int ID)
-    {
+    public void EditAuftrag(int ID) {
 
     }
 
-    public ArrayList LoadRechnungList()
-    {
+    public ArrayList LoadRechnungList() {
 
         return rechnungliste;
     }
 
-    public void LoadRechnung(int ID)
-    {
+    public void LoadRechnung(int ID) {
 
     }
 
-    public void NewRechnung()
-    {
+    public void NewRechnung() {
 
+    }
+    public void db_close(){
+        try{
+            connection.close();
+        }catch (Exception e){
+
+        }
+    }
+
+    /* Installationsroutine*/
+    public void db_install(){
+        String query0 = "CREATE DATABASE IF NOT EXISTS `" + dbName + "`";
+        String query1 = "USE `" + dbName + "`";
+        String query2 = "CREATE TABLE IF NOT EXISTS`stammdaten` (" +
+                "  `id` tinyint(1) NOT NULL," +
+                "  `firma` varchar(100) NOT NULL," +
+                "  `vorname` varchar(50) NOT NULL," +
+                "  `nachname` varchar(50) NOT NULL," +
+                "  `strasse` varchar(100) NOT NULL," +
+                "  `hsnr` varchar(20) NOT NULL," +
+                "  `plz` varchar(10) NOT NULL," +
+                "  `ort` varchar(50) NOT NULL," +
+                "  `telefon` varchar(50) NOT NULL," +
+                "  `telefax` varchar(50) NOT NULL," +
+                "  `web` varchar(50) NOT NULL," +
+                "  `email` varchar(50) NOT NULL," +
+                "  `bankname` varchar(50) NOT NULL," +
+                "  `kontoinhaber` varchar(50) NOT NULL," +
+                "  `bic` varchar(50) NOT NULL," +
+                "  `iban` varchar(50) NOT NULL," +
+                "  `steuernummer` varchar(50) NOT NULL" +
+                ") ENGINE=MyISAM DEFAULT CHARSET=utf8";
+        String query3 = "ALTER TABLE `stammdaten`" +
+                "  ADD PRIMARY KEY (`id`)";
+        String query4 = "ALTER TABLE `stammdaten`" +
+                "  MODIFY `id` tinyint(1) NOT NULL AUTO_INCREMENT";
+        String query5 = "CREATE TABLE IF NOT EXISTS `kunden` (" +
+                "  `id` int(11) NOT NULL," +
+                "  `firma` int(11) NOT NULL," +
+                "  `strasse` varchar(50) NOT NULL," +
+                "  `hsnr` varchar(20) NOT NULL," +
+                "  `plz` varchar(20) NOT NULL," +
+                "  `ort` varchar(50) NOT NULL," +
+                "  `telefon` varchar(50) NOT NULL," +
+                "  `telefax` varchar(50) NOT NULL," +
+                "  `web` varchar(50) NOT NULL," +
+                "  `email` varchar(50) NOT NULL," +
+                "  `ap_anrede` enum('Herr','Frau') NOT NULL DEFAULT 'Herr'," +
+                "  `ap_vorname` varchar(50) NOT NULL," +
+                "  `ap_nachname` varchar(50) NOT NULL," +
+                "  `ap_telefon` varchar(50) NOT NULL," +
+                "  `ap_email` varchar(50) NOT NULL," +
+                "  `stdsatz` decimal(10,2) NOT NULL" +
+                ") ENGINE=MyISAM DEFAULT CHARSET=utf8";
+        String query6 = "ALTER TABLE `kunden`" +
+                "  ADD PRIMARY KEY (`id`)";
+        String query7 = "ALTER TABLE `kunden`" +
+                "  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT";
+
+        try{
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+            statement.addBatch(query0);
+            statement.addBatch(query1);
+            statement.addBatch(query2);
+            statement.addBatch(query3);
+            statement.addBatch(query4);
+            statement.addBatch(query5);
+            statement.addBatch(query6);
+            statement.addBatch(query7);
+            statement.executeBatch();
+            connection.commit();
+            statement.close();
+        }catch (Exception e){
+
+        }
     }
 }
--- src/Abrechnungssoftware/Main.java
+++ src/Abrechnungssoftware/Main.java
@@ -1,8 +1,14 @@
 package Abrechnungssoftware;
 
 import Abrechnungssoftware.Gui.MainController;
-import Abrechnungssoftware.Verarbeitung.Auftrag;
 import javafx.application.Application;
+import Abrechnungssoftware.DB.DB_CON;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Properties;
 
 public class Main
 {
@@ -10,7 +16,47 @@ public class Main
 
     public static void main(String[] args)
     {
+        File file = new File("system.ini");
+        boolean exists = file.exists();
+        boolean install = false;
+        if (file.exists() && file.isFile()) {
+
+        }else{
+            String host,benutzer,password,datenbank;
+            host = JOptionPane.showInputDialog("Datenbankserveradresse ?");
+            benutzer = JOptionPane.showInputDialog("Benutzername ?");
+            password = JOptionPane.showInputDialog("Passwort ?");
+            datenbank = JOptionPane.showInputDialog("Datenbank ?");
+
+            Writer writer = null;
+            try {
+                writer = new FileWriter("system.ini");
+                Properties prop1 = new Properties( System.getProperties() );
+                prop1.setProperty( "HOST", host );
+                prop1.setProperty( "USER", benutzer );
+                prop1.setProperty( "PWD", password );
+                prop1.setProperty( "DATENBANK", datenbank );
+                prop1.store( writer, "Keine manuellen Aenderungen vornehmen!" );
+            } catch (Exception e){
+
+            }finally{
+                try {
+                    writer.close();
+                } catch ( Exception e ) {
+
+                }
+
+            }
+            DB_CON inst_db = new DB_CON();
+            inst_db.db_open();
+            inst_db.db_install();
+            inst_db.db_close();
+        }
+
         Application.launch(MainController.class, args);
+
+
+
 
     }
 }
